9c0fb56e25a7a6141e3a95903f4f4fcc40ccf234
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-09 17:41:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <h1>Amanda Pipich website</h1>
+    </div>
+    <footer className="myCode"><small>Open-source code by Amanda Pipich</small></footer>
+    </div>
+  );
+}
