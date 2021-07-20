6ebf7bf6f15bd8a6f58bb3ba81b36c850f9a544d
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-20 13:46:39 -0400

    created shell of nav bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,17 +3,35 @@ import "./Navigation.css";
 
 export default function Navigation() {
 return (
-//should do bootstrap nav bar to start
 
-<nav className="navbar">
-<ul>
-<li className="home"><a href="/">Home</a></li>
-<li className="about"><a href="/">About</a></li>
-<li className="contact"><a href="/">Contact</a></li>
-<li className="projects"><a href="/">Projects</a></li>
-<li className="photography"><a href="/">Photography</a></li>
-  </ul>
+<nav className="navbar fixed-top navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Projects</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
+
+
+
+
+
+
+
 )
 
 }
