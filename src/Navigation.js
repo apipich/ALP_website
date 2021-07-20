88e34a7d4932466a9ce3c7a29db83b61cebe5fbc
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-20 16:57:04 -0400

    updated nav bar with page destinations

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,31 +1,21 @@
 import React from "react";
 import "./Navigation.css";
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Navigation() {
 return (
-
-<nav className="navbar fixed-top navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Home</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Projects</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+  <Navbar fixed="top" bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Amanda Pipich</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Projects</Nav.Link>
+      <Nav.Link href="#pricing">Photography</Nav.Link>
+      <Nav.Link href="#pricing">About</Nav.Link>
+      <Nav.Link href="#pricing">Contact</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
 
 
 
