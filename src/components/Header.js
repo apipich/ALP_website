a6111e74d43a5ad42989b14b96543e9431d2029e
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-29 17:07:53 -0400

    adjusted nav bar text to right

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import { LinkContainer } from "react-router-bootstrap";
 
 export default function Header() {
   return (
-    <Navbar className="justify-content-end" bg="light" expand="lg">
+    <Navbar bg="light" expand="lg">
       <LinkContainer to="/">
         <Navbar.Brand>Amanda Pipich</Navbar.Brand>
       </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="justify-content-end">
           <LinkContainer to="/Contact">
             <Nav.Link>Contact</Nav.Link>
           </LinkContainer>
