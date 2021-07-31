c8cb9450e8faa9dec637edbccc4d10f67f1e3f7a
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-07-31 11:50:27 -0400

    began adding Project 3 (dictionary)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectOne from "./ProjectOne.js";
 import ProjectTwo from "./ProjectTwo.js";
+import ProjectThree from "./ProjectThree.js";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -15,6 +16,9 @@ export default function Projects() {
           </Col>
           <Col>
             <ProjectTwo />
+          </Col>
+          <Col>
+            <ProjectThree />
           </Col>
         </Row>
       </Container>
