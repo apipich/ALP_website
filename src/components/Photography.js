9ec816fab9455dfb3096cba0822791ece273ac67
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-01 10:10:29 -0400

    added initial photo to photo gallery

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,9 +1,18 @@
 import React from "react";
+import napa from "./napa.JPG";
 
 export default function Photography() {
   return (
-    <div className="photography">
+    <div className="Container">
       <h1>photography</h1>
+      <div className="photography">
+        <h3>image gallery</h3>
+      </div>
+      <div className="Row">
+        <div className="col-4">
+          <img src={napa} alt="Napa, CA" class="img-thumbnail" />
+        </div>
+      </div>
     </div>
   );
 }
