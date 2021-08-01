d45d5e6ac48fe3f773d0998da195f8cb29a336b3
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-01 11:02:55 -0400

    adjusted col/row

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,42 +8,36 @@ export default function Projects() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-4">
-          <div className="projectOne">
-            <h5>Javascript Weather Project</h5>
-            <a
-              href="https://vibrant-lovelace-115a07.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={project1} width="300" alt="javascript weather" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="col-4">
-        <div className="projectTwo">
-          <h5>React Weather Project</h5>
+        <div className="col-sm">
+          <h5>Javascript Weather Project</h5>
           <a
-            href="https://distracted-heyrovsky-8e212e.netlify.app/"
+            href="https://vibrant-lovelace-115a07.netlify.app/"
             target="_blank"
             rel="noreferrer"
           >
-            <img src={project2} width="300" alt="react weather" />
+            <img src={project1} width="300" alt="javascript weather" />
           </a>
         </div>
       </div>
-      <div className="col-4">
-        <div className="projectThree">
-          <h5>React Dictionary Project</h5>
-          <a
-            href="https://nervous-bardeen-829a9e.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={project3} width="300" alt="react dictionary" />
-          </a>
-        </div>
+      <div className="col-sm">
+        <h5>React Weather Project</h5>
+        <a
+          href="https://distracted-heyrovsky-8e212e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={project2} width="300" alt="react weather" />
+        </a>
+      </div>
+      <div className="col-sm">
+        <h5>React Dictionary Project</h5>
+        <a
+          href="https://nervous-bardeen-829a9e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={project3} width="300" alt="react dictionary" />
+        </a>
       </div>
     </div>
   );
