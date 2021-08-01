06027ef6f125c5fe75995ab8df733e66dbe375de
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-01 10:20:24 -0400

    added a new photo

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,14 +1,14 @@
 import React from "react";
 import napa from "./napa.JPG";
 import paris from "./paris.JPG";
+import castle from "./castle.JPG";
 
 export default function Photography() {
   return (
-    <div className="Container">
+    <div className="photography">
       <h1>photography</h1>
-      <div className="photography">
-        <h3>image gallery</h3>
-      </div>
+      <h3>image gallery</h3>
+      <div className="Container"></div>
       <div className="Row">
         <div className="col-4">
           <img src={napa} alt="Napa, CA" class="img-thumbnail" />
@@ -17,6 +17,11 @@ export default function Photography() {
       <div className="Row">
         <div className="col-4">
           <img src={paris} alt="Paris, France" class="img-thumbnail" />
+        </div>
+      </div>
+      <div className="Row">
+        <div className="col-4">
+          <img src={castle} alt="Paris, France" class="img-thumbnail" />
         </div>
       </div>
     </div>
