176a42988f815be2ad16a0cea07ff8191e93eee1
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-01 16:05:16 -0400

    moved photography images to public folder

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,25 +1,22 @@
 import React from "react";
-import napa from "./napa.JPG";
-import paris from "./paris.JPG";
-import castle from "./castle.JPG";
 import "./Photography.css";
 
 export default function Photography() {
   return (
     <div className="photography">
       <br />
-      <h2>photos taken by me</h2>
+      <h2>photos by amanda</h2>
       <br />
       <div className="container">
         <div className="row">
           <div className="col-4">
-            <img src={napa} alt="Napa, CA" class="img-thumbnail" />
+            <img src="napa.JPG" alt="Napa, CA" class="img-thumbnail" />
           </div>
           <div className="col-4">
-            <img src={paris} alt="Paris, France" class="img-thumbnail" />
+            <img src="paris.JPG" alt="Paris, France" class="img-thumbnail" />
           </div>
           <div className="col-4">
-            <img src={castle} alt="Paris, France" class="img-thumbnail" />
+            <img src="castle.JPG" alt="Paris, France" class="img-thumbnail" />
           </div>
         </div>
       </div>
