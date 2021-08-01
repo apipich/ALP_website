e44cc81864c4e66dd5a39c35e139886773e1b863
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-01 09:56:43 -0400

    adjusted rows and columns, fixed font family

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,54 +2,49 @@ import React from "react";
 import project1 from "./project1.png";
 import project2 from "./project2.png";
 import project3 from "./project3.png";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./Projects.css";
 
 export default function Projects() {
   return (
-    <div className="Projects">
-      <Container>
-        <Row>
-          <Col>
-            <div className="projectOne">
-              <h5>Javascript Weather Project</h5>
-              <a
-                href="https://vibrant-lovelace-115a07.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={project1} width="300" alt="javascript weather" />
-              </a>
-            </div>
-          </Col>
-          <Col>
-            <div className="projectTwo">
-              <h5>React Weather Project</h5>
-              <a
-                href="https://distracted-heyrovsky-8e212e.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={project2} width="300" alt="react weather" />
-              </a>
-            </div>
-          </Col>
-          <Col>
-            <div className="projectThree">
-              <h5>React Dictionary Project</h5>
-              <a
-                href="https://nervous-bardeen-829a9e.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={project3} width="300" alt="react dictionary" />
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+    <div className="Container">
+      <div className="Row">
+        <div className="col-4">
+          <div className="projectOne">
+            <h5>Javascript Weather Project</h5>
+            <a
+              href="https://vibrant-lovelace-115a07.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={project1} width="300" alt="javascript weather" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-4">
+        <div className="projectTwo">
+          <h5>React Weather Project</h5>
+          <a
+            href="https://distracted-heyrovsky-8e212e.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project2} width="300" alt="react weather" />
+          </a>
+        </div>
+      </div>
+      <div className="col-4">
+        <div className="projectThree">
+          <h5>React Dictionary Project</h5>
+          <a
+            href="https://nervous-bardeen-829a9e.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={project3} width="300" alt="react dictionary" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
