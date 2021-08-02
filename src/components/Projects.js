3b775a14d15b2785fd8c0997d82ccfc06a284060
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-02 19:57:13 -0400

    added repository link to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -41,6 +41,18 @@ export default function Projects() {
           </div>
         </div>
       </div>
+      <footer className="myCode">
+        <small>
+          <a
+            href="https://github.com/apipich/ALP_website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Amanda Pipich
+        </small>
+      </footer>
     </div>
   );
 }
