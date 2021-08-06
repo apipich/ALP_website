de274c22576e7861d744be84faa0b2c5c3bf1c32
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-06 16:25:09 -0400

    increased font of my name blah blah

--- src/App.js
+++ src/App.js
@@ -9,28 +9,15 @@ export default function App() {
         <p>
           I'm a full time advertising professional in New York, NY. My love of
           technology and learning new software has brought me here, and looking
-          to take on freelance projects at any moment. Let's connect!
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, an tibique recteque partiendo quo. Causae
-          adipisci in sit, qui at eros intellegam conclusionemque, est ad vide
-          scribentur. Sed ut delenit oporteat corrumpit, ei sit diam dicam
-          quando, no congue putent sed.
-          <br />
-          <span className="myCode">
-            <small>
-              Photo and
-              <a
-                href="https://github.com/apipich/ALP_website"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Open-source code
-              </a>{" "}
-              by Amanda Pipich
-            </small>
-          </span>
+          to take on freelance projects at any moment. Let's connect! Photo and{" "}
+          <a
+            href="https://github.com/apipich/ALP_website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Amanda Pipich
         </p>
       </div>
       <img src="Hoboken.png" alt="hoboken" width="100%" className="hoboken" />
--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,21 @@ import React from "react";
 export default function About() {
   return (
     <div className="About">
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, an tibique recteque partiendo quo. Causae
-        adipisci in sit, qui at eros intellegam conclusionemque, est ad vide
-        scribentur. Sed ut delenit oporteat corrumpit, ei sit diam dicam quando,
-        no congue putent sed. Ad posse soluta iudicabit eum, has offendit
-        oportere intellegam in, ex pro exerci laudem accommodare. No pri debitis
-        dolores comprehensam. Eam et alienum accommodare. An ridens putant
-        insolens duo, duo simul aliquip copiosae eu. Mea in nonumes
-        definitiones, quod elaboraret sea te. Semper numquam ad duo. Quando
-        melius moderatius sea no, essent alterum eloquentiam sed ad, has at modo
-        debet nostrud.
-      </p>
+      <div className="container">
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, an tibique recteque partiendo quo. Causae
+          adipisci in sit, qui at eros intellegam conclusionemque, est ad vide
+          scribentur. Sed ut delenit oporteat corrumpit, ei sit diam dicam
+          quando, no congue putent sed. Ad posse soluta iudicabit eum, has
+          offendit oportere intellegam in, ex pro exerci laudem accommodare. No
+          pri debitis dolores comprehensam. Eam et alienum accommodare. An
+          ridens putant insolens duo, duo simul aliquip copiosae eu. Mea in
+          nonumes definitiones, quod elaboraret sea te. Semper numquam ad duo.
+          Quando melius moderatius sea no, essent alterum eloquentiam sed ad,
+          has at modo debet nostrud.
+        </p>
+      </div>
     </div>
   );
 }
