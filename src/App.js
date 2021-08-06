4dc0e49529075daa5d5f7ecab5cf08b58a43581f
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-06 16:51:53 -0400

    added more description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export default function App() {
       <div className="container">
         <h1>amanda pipich</h1>
         <p>
-          I'm a full time advertising professional in New York, NY. My love of
+          I'm a full time advertising professional in Hoboken (below, my
+          beautiful city, right across the river from New York). My love of
           technology and learning new software has brought me here, and looking
           to take on freelance projects at any moment. Let's connect! Photo and{" "}
           <a
