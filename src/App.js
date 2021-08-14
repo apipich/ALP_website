45ef15595a04f693466ed24dc5b87f37729242e7
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-14 16:13:18 -0400

    updated homepage paragraph

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h4>Hi, I'm</h4>
-        <h1>amanda pipich</h1>
+        <h1>amanda pipich.</h1>
+        <h4>
+          I'm an advertisng professional by day and a web developer in-training
+          by night. Welcome to my [work in progress] portfolio. Based in
+          Hoboken, NJ.
+        </h4>
       </div>
       <img src="Hoboken.png" alt="hoboken" width="100%" className="hoboken" />
     </div>
