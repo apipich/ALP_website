29223c54a20a8ae3fcb804fc3d7668547744720f
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-14 16:09:15 -0400

    added new font to homepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <h4>Hi, I'm</h4>
         <h1>amanda pipich</h1>
       </div>
       <img src="Hoboken.png" alt="hoboken" width="100%" className="hoboken" />
