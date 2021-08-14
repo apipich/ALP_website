adad2036673522326c0cec51eb5680463ab0ddad
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-14 16:00:02 -0400

    moved intro to about me section

--- src/components/About.js
+++ src/components/About.js
@@ -7,16 +7,18 @@ export default function About() {
         <br />
         <h1>about me</h1>
         <p>
-          Lorem ipsum dolor sit amet, an tibique recteque partiendo quo. Causae
-          adipisci in sit, qui at eros intellegam conclusionemque, est ad vide
-          scribentur. Sed ut delenit oporteat corrumpit, ei sit diam dicam
-          quando, no congue putent sed. Ad posse soluta iudicabit eum, has
-          offendit oportere intellegam in, ex pro exerci laudem accommodare. No
-          pri debitis dolores comprehensam. Eam et alienum accommodare. An
-          ridens putant insolens duo, duo simul aliquip copiosae eu. Mea in
-          nonumes definitiones, quod elaboraret sea te. Semper numquam ad duo.
-          Quando melius moderatius sea no, essent alterum eloquentiam sed ad,
-          has at modo debet nostrud.
+          I'm a full time advertising professional in Hoboken (my beautiful
+          city, right across the river from New York that appears on my
+          homepage). My love of technology and learning new software has brought
+          me here, building my website brick by brick. Let's connect!
+          <a
+            href="https://github.com/apipich/ALP_website"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Amanda Pipich.
         </p>
       </div>
     </div>
