a4a4200b973986dc99ee24a778e98d76353554e6
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-08-14 16:05:36 -0400

    updated homepage and header font

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>amanda pipich</h1>
-        <p>
-          I'm a full time advertising professional in Hoboken (below, my
-          beautiful city, right across the river from New York). My love of
-          technology and learning new software has brought me here, building my
-          website brick by brick. Let's connect! Photo and{" "}
-          <a
-            href="https://github.com/apipich/ALP_website"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by Amanda Pipich
-        </p>
       </div>
       <img src="Hoboken.png" alt="hoboken" width="100%" className="hoboken" />
     </div>
